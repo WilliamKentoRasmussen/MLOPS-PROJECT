a213402baf24cc9ac82b4ccaa71562c87f45a7c3
WilliamKentoRasmussen <s245310@dtu.dk>
2026-01-15 11:08:56 +0100

    Added gitignore

--- src/main_project/download_kaggle_dataset.py
+++
@@ -1,9 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("paultimothymooney/chest-xray-pneumonia")
-
-print("Path to dataset files:", path)
-
-
-#/Users/kento/.cache/kagglehub/datasets/paultimothymooney/chest-xray-pneumonia/versions/2
